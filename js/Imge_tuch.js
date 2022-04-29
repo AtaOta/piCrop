0bf8e393697f6d9ae91c8231d023c455df863e69
AtaOta <ataota.org@gmail.com>
2022-04-29 17:21:24 +0530

    PICROP, VERSION-0.0.4

--- js/Imge_tuch.js
+++ js/Imge_tuch.js
@@ -1,6 +1,5 @@
 const Cropper_Wrapper = document.querySelector(".Cropper_Wrapper");
 const Cropping_IMG = document.querySelector('.Cropping_IMG');
-let Ismoving = false
 let leWrapper = Cropper_Wrapper.getBoundingClientRect();
 
 let CWheight = Cropper_Wrapper.clientHeight;
@@ -18,10 +17,7 @@ function mousedown(e){
     window.addEventListener("mousemove", mousemove);
     window.addEventListener("mouseup", mouseup);
 
-    // console.warn(prevX, prevY)
-
     function mousemove(e){
-
         let newX = prevX - e.clientX;
         let newY = prevY - e.clientY;
         let leImg = Cropping_IMG.getBoundingClientRect();
@@ -31,6 +27,7 @@ function mousedown(e){
         prevX = e.clientX;
         prevY = e.clientY;
     }
+
     function mouseup(){
         window.removeEventListener("mousemove", mousemove);
         window.removeEventListener("mouseup", mouseup);
@@ -42,17 +39,17 @@ function touchstart(e){
     let touch = e.touches[0];
     let prevX = touch.clientX;
     let prevY = touch.clientY;
+
     window.addEventListener('touchmove', touchmove);
     window.addEventListener('touchend', touchend);
 
     function touchmove(e){
-
         let touch = e.touches[0];
         let newX = prevX - touch.clientX;
         let newY = prevY - touch.clientY;
         let leImg = Cropping_IMG.getBoundingClientRect();
-        Cropping_IMG.style.left = `${leImg.left - newX -leWrapper.left}px`;
-        Cropping_IMG.style.top = `${leImg.top - newY -leWrapper.top}px`;
+        Cropping_IMG.style.left = `${leImg.left - newX - leWrapper.left}px`;
+        Cropping_IMG.style.top = `${leImg.top - newY - leWrapper.top}px`;
 
         prevX = touch.clientX;
         prevY = touch.clientY;
@@ -63,3 +60,8 @@ function touchstart(e){
         window.removeEventListener('touchend', touchend);
     }
 }
+
+window.addEventListener('resize', ()=>{
+    Cropping_IMG.style.width = `${Cropper_Wrapper.clientWidth}px`;
+    leWrapper = Cropper_Wrapper.getBoundingClientRect();
+})
